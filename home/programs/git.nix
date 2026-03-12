cb52f213fb8a6dde0c6a976ae0aac2f0f3985ddd
Martin Trenker <martin@pacabytes.io>
2026-03-12 12:15:47 +0100

    fix: enhance git configuration with additional aliases and improved settings

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -8,33 +8,94 @@
       user.name  = "Martin Trenker";
       user.email = "martin@pacabytes.io";
       init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-      rebase.autoStash = true;
+
+      color.ui = "auto";
 
       core = {
-        editor    = "nvim";
-        autocrlf  = "input";
-        pager     = "delta";
+        editor       = "nvim";
+        autocrlf     = "input";
+        pager        = "delta";
+        preloadindex = true;
+        fscache      = true;
       };
 
       # https://dandavison.github.io/delta/
       delta = {
-        navigate    = true;
+        navigate     = true;
         line-numbers = true;
         side-by-side = false;
       };
 
-      merge.conflictstyle = "zdiff3";
-      diff.algorithm      = "histogram";
+      merge = {
+        tool          = "vimdiff";
+        conflictstyle = "zdiff3";
+      };
+
+      diff = {
+        tool      = "vimdiff";
+        algorithm = "histogram";
+      };
+
+      push = {
+        default          = "simple";
+        autoSetupRemote  = true;
+      };
+
+      pull.rebase = true;
+
+      rebase = {
+        autoStash  = true;
+        autoSquash = true;
+      };
+
+      status = {
+        showUntrackedFiles = "all";
+        submoduleSummary   = true;
+      };
+
+      maintenance.auto = true;
 
       url."git@github.com:".insteadOf = "https://github.com/";
+
+      "credential \"https://dev.azure.com/dglecom\"".helper = "!/home/martin/dev/douglas/credentials-helper.sh dglecom";
+      "credential \"https://dev.azure.com/dglas\"".helper   = "!/home/martin/dev/douglas/credentials-helper.sh dglas";
+
       alias = {
-        st   = "status -sb";
-        lg   = "log --oneline --graph --decorate --all";
-        undo = "reset --soft HEAD~1";
-        wip  = "!git add -A && git commit -m 'wip'";
+        # basics
+        co  = "checkout";
+        ci  = "commit";
+        br  = "branch";
+        pl  = "pull";
+        ps  = "push";
+        st  = "status -sb";
+        df  = "diff";
+
+        # logging
+        lg     = "log --oneline --graph --decorate --all";
+        ll     = "log --oneline --graph --decorate --all -10";
+        last   = "log -1 HEAD";
+        lol    = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+        # undoing
+        undo    = "reset --soft HEAD~1";
+        unstage = "reset HEAD --";
+        amend   = "commit --amend --no-edit";
+        fixup   = "commit --fixup";
+
+        # branching
+        recent  = "branch --sort=-committerdate";
+        cleanup = "!git branch --merged | grep -v '\\*\\|main\\|master\\|develop' | xargs -n 1 git branch -d";
+
+        # stashing
+        save = "stash push -m";
+        pop  = "stash pop";
+
+        # wip
+        wip   = "!git add -A && git commit -m 'wip'";
         unwip = "!git log --oneline -1 | grep -q 'wip' && git reset HEAD~1";
+
+        # sync
+        sync = "!git fetch origin && git rebase origin/$(git branch --show-current)";
       };
     };
 
